9daa5ec7d4852e3f128f5b155b9d515b7566d535
bergad37 <bergad37@gmail.com>
2023-07-12 15:49:20 +0200

    Inital typescript commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let size = 10;
+if (size < 20) {
+    size++;
+}
+console.log(size);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let size: number=10;
+
+if(size<20){
+size++;
+}
+console.log(size);
+
