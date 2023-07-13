88f69fc760538925bd4515a3720c859de2726f36
bergad37 <bergad37@gmail.com>
2023-07-13 14:53:00 +0200

    Type alias

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-let employee:{
+type Employee={
   readonly id: number,
-  name:string,
-  retire:(date:Date)=>void   //use arrow function notation
-   
-}={ 
+    name:string,
+    retire:(date:Date)=>void   //use arrow function notation 
+}
+
+let employee: Employee={    // We defined and used our own type
   id: 1,
   name:'Gad',
   retire: (date:Date)=>{
